cebad22a7b073e989028532f7fe3bd60fc8ae5e3
Jing <boundino@gmail.com>
2019-10-25 13:18:15 -0400

    clear header softlinks

--- CrossSection/fitXvary/run_fitXvary.sh
+++ CrossSection/fitXvary/run_fitXvary.sh
@@ -17,12 +17,12 @@ inputmc_a_nonprompt=/raid5/data/wangj/BntupleRun2018/mva_output_20190808ptdep/nt
 inputmc_b_nonprompt=/raid5/data/wangj/BntupleRun2018/mva_output_20190808ptdep/ntmix_20190808_Bfinder_20190712_Hydjet_Pythia8_NonPromptXRho_1033p1_pt6tkpt0p9dls0_pthatweight_trainX_20190808ptdep_sideband_tktk0p2_BDT_BDTD_BDTG_BDTF_LD_15p0_50p0_0-10-1-2-9_1bin.root
 
 name=trainX_20190808ptdep_sideband_tktk0p2_15p0_50p0_0-10-1-2-9
-g++ getfname.cc $(root-config --libs --cflags) -g -o getfname_${tmp}.exe || { rm *_${tmp}.exe 2> /dev/null ; exit 1 ; }
+g++ getfname.cc -I../../includes/ $(root-config --libs --cflags) -g -o getfname_${tmp}.exe || { rm *_${tmp}.exe 2> /dev/null ; exit 1 ; }
 kinematic=$(./getfname_${tmp}.exe $ptmin $ptmax $centmin $centmax $ymin $ymax)
 rm getfname_${tmp}.exe
 
-g++ fitXvary.C $(root-config --libs --cflags) -lRooFit -lRooFitCore -lRooStats -g -o fitXvary.exe || { rm *_${tmp}.exe 2> /dev/null ; exit 1 ; }
-g++ drawfitXvary.C $(root-config --libs --cflags) -lRooFit -lRooFitCore -lRooStats -g -o drawfitXvary.exe || { rm *_${tmp}.exe 2> /dev/null ; exit 1 ; }
+g++ fitXvary.C -I../../includes/ $(root-config --libs --cflags) -lRooFit -lRooFitCore -lRooStats -g -o fitXvary.exe || { rm *_${tmp}.exe 2> /dev/null ; exit 1 ; }
+g++ drawfitXvary.C -I../../includes/ $(root-config --libs --cflags) -lRooFit -lRooFitCore -lRooStats -g -o drawfitXvary.exe || { rm *_${tmp}.exe 2> /dev/null ; exit 1 ; }
 
 echo -e "----------------------------------------"
 echo -e "==> File directory: \e[4m$name\e[0m"
--- CrossSection/fitXvary/xjjrootuti.h
+++
@@ -1,1 +0,0 @@
-../xjjrootuti.h
